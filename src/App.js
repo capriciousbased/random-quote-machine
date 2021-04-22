f27fb8c85fd041cb2c8b05f7ca277a2fe39a34bc
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-04-23 01:15:18 +0200

    data fetching function added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [randomQuotes, setrandomQuotes] = useState([]);
+
+  async function fetchQuote() {
+    const response = await fetch("https://type.fit/api/quotes");
+    const data = await response.json();
+    setQuotes(data);
+    let randIndex = Math.floor(Math.random() * data.length);
+    setrandomQuotes(data[randIndex]);
+  }
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="quote-box">
+      <h1>Random Quote Machine</h1>
+      {quotes.map((quote) => (
+        <>
+          <p>{quote.text}</p>
+          <p>{quote.author}</p>
+        </>
+      ))}
     </div>
   );
 }
